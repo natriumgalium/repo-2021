8a2409766d1d26657f66c4ccc11a397c1e6fafd9
natriumgalium <67674119+natriumgalium@users.noreply.github.com>
2021-01-09 09:52:07 +0700

    ganti rencana 1 dengan beli pc baru

--- rencana2021.cpp
+++ rencana2021.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 
 cout<<"Rencana 2021"<<endl;
-cout<<"1. Membeli laptop baru"<<endl;
+cout<<"1. Membeli pc baru"<<endl;
 cout<<"2. Ikut pimnas"<<endl;
 cout<<"3. Ikut organisasi"<<endl;
 
